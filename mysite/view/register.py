133848329c46749152341f5eda7c87464050393b
jak010 <bluetoon@naver.com>
2021-09-20 23:29:39 +0900

    swagger_auto_schema get&&post 예제 추가

--- mysite/view/register.py
+++ mysite/view/register.py
@@ -14,27 +14,47 @@ from core.serializer.accountserialize import AccountSerializer
 
 
 class AccountRegisterView(APIView):
-    # GET
-    test_param = openapi.Parameter('test', openapi.IN_QUERY,
-                                   description="test manual param",
-                                   type=openapi.TYPE_NUMBER)
 
     @swagger_auto_schema(
         operation_id='유저 목록조회',
         operation_description="유저 데이터 조회입니다",
-        manual_parameters=[test_param],
-        responses={200: openapi.Response('', AccountSerializer(many=True))}
 
+        manual_parameters=[
+            openapi.Parameter('nickname', openapi.IN_QUERY, required=True, description="회원 nickname",
+                              type=openapi.TYPE_NUMBER),
+            openapi.Parameter('email', openapi.IN_QUERY, required=True, description="회원 nickname",
+                              type=openapi.TYPE_NUMBER),
+            openapi.Parameter('password', openapi.IN_QUERY, required=True, description="회원 nickname",
+                              type=openapi.TYPE_NUMBER),
+        ],
+
+        responses={
+            200: openapi.Response(
+                description="정상적인 경우",
+                schema=AccountSerializer(many=True)
+            )
+        }
     )
     def get(self, request):
         return Response({"message": "ok"})
 
     @swagger_auto_schema(
-        operation_id='유저 생성하기',
-        operation_description=
-        """
-            @POST, ACCOUNT는 유저 생성입니다.
-        """
+        operation_id="유저 생성하기",
+        operation_description="유저 생성하기",
+
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'reg_nickname': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+                'reg_email': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+                'reg_password': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+            },
+            required=['reg_nickname', 'reg_email', 'reg_password']
+        ),
+        responses={
+            200: 'ok',
+        }
+
     )
     def post(self, request):
         serializer = AccountSerializer(data=request.POST)
